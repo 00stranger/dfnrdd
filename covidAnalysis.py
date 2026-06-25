d87a774ffb846467e51c319236cd08624782b837
Manoj Gurung <gurungm109@gmail.com>
2026-06-25 15:53:48 -0500

    replaced sql transformation codes to spark transformation codes

--- covidAnalysis.py
+++ covidAnalysis.py
@@ -1,35 +1,39 @@
-
+from pyspark.sql import functions as F
 from pyspark.sql.functions import column, sum, max
-
+from pyspark.sql.window import Window
 def covid_data_analysis(spark,path_to_covidcase):
     covidDF = spark.read.csv(path_to_covidcase, header=True, inferSchema=True)
+    #now changing the name of column 'infection case' to infection_source
     covid_renameDF= covidDF.withColumnRenamed("infection_case", "infection_source")
-    #covid_renameDF.show()
-    covid_renameDF.createOrReplaceTempView("covidtable")
-    covidSql = spark.sql("select province, city, infection_source, confirmed from covidtable")
-    #covidSql.show()
+    #display only selected columns from our data
+    covidDF_smallview = covid_renameDF.select(["province", "city", "infection_source", "confirmed"])
+    covidDF_smallview.show()
+
+    #changing the datatype of column confirmed to Integer by typecasting
     covid_renameDF= covid_renameDF.withColumn("confirmed", column("confirmed").cast("integer"))
-    covid_renameDF.printSchema()
+    #displaying TotalConfirmed and max confirmed from province
+    covid_confirmedDF = covid_renameDF.groupBy("province", "city")\
+                        .agg(
+                            sum("confirmed").alias("TotalConfirmed"),
+                            max("confirmed").alias("MaxFromOneConfirmedCase")
+                            )
+    covid_confirmedDF.show()
 
-    covidSql1 = spark.sql('select province, city, sum(confirmed) as TotalConfirmed,\
-                          max(confirmed) as MaxFromOneConfirmedCase\
-                          From covidtable Group By province, city\
-                          Order by TotalConfirmed ASC')
-    #covidSql1.show(156, truncate=False)
-    covidSql1.show()
-    rankSql = spark.sql('select * from (select province, city,sum(confirmed) as TotalConfirmed,\
-                        Dense_RANK() over (order by sum(confirmed) desc) as case_rank\
-                        From covidtable\
-                        Group By province,city)\
-                        Where case_rank <= 2')
-    rankSql.show()
+    #now we display the province and city based on 2 highest total no. of confirmed covid cases.
+    windowSpec = Window.orderBy(F.desc("TotalConfirmed"))
+    covid_renameDF.groupBy("province", "city")\
+                  .agg(F.sum("confirmed").alias("TotalConfirmed"))\
+                   .withColumn("case_rank", F.dense_rank().over(windowSpec))\
+                    .filter(F.column("case_rank") <= 2)\
+                    .show()
 
-    daeguSql = spark.sql('Select * from (select province, city, sum(confirmed) as TotalConfirmed\
-                         From covidtable\
-                         Group BY province,city)\
-                         where province="Daegu" And TotalConfirmed > 10')
-    daeguSql.show()
+    #time to display only covid cases specific to Daegu province with more than 10 total confirmed covid cases
+    covid_renameDF.groupBy("province", "city")\
+                  .agg(F.sum("confirmed").alias("TotalConfirmed"))\
+                  .filter((F.column("province")=="Daegu") & (F.column("TotalConfirmed") > 10))\
+                  .show()
+
     #let's practice dropping some columns now
-    drop_list = [" case_id", "latitude", "longitude"]    #case_id has a space because that's how it's named in excel file 
+    drop_list = [" case_id", "latitude", "longitude"]    #case_id has a space because that's how it's named in excel file
     df_dropped = covid_renameDF.drop(*drop_list)
     df_dropped.show()
